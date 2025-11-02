a128e28a8b069c63a59ce97b2a1ed7d4f744ab0a
Christoph <c.wagner.germany@gmail.com>
2025-11-02 14:18:45 +0100

    previews

--- src/app/pages/applications/applications.data.ts
+++ src/app/pages/applications/applications.data.ts
@@ -10,14 +10,6 @@ export const PRODUCT_PATH = 'applications';
 
 export const SECTION: SectionItem[] = [
   {
-    productId: 'white-test',
-    productName: 'white-test',
-    productDescription:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    productLogoFilename: 'white-test.webp',
-    productLogoAltText: 'white-test',
-  },
-  {
     productId: 'chat',
     productName: 'Chat App',
     productDescription:
