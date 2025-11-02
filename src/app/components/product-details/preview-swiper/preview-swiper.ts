c36b731a4685dac121a67dd8dec0d469bfcffc70
Christoph <c.wagner.germany@gmail.com>
2025-11-02 14:36:12 +0100

    Pfad

--- src/app/components/product-details/preview-swiper/preview-swiper.ts
+++ src/app/components/product-details/preview-swiper/preview-swiper.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-preview-swiper',
   imports: [],
   templateUrl: './preview-swiper.html',
-  styleUrl: './preview-swiper.scss'
+  styleUrl: './preview-swiper.scss',
 })
 export class PreviewSwiper {
-
+  @Input() productPreviewsPath!: string;
 }
