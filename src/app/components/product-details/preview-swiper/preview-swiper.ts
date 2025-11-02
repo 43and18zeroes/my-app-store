fd7af907bc6e1c997401a7c774399a847d1e25fd
Christoph <c.wagner.germany@gmail.com>
2025-11-02 14:51:36 +0100

    Pfad2

--- src/app/components/product-details/preview-swiper/preview-swiper.ts
+++ src/app/components/product-details/preview-swiper/preview-swiper.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { DeviceService } from '../../../services/device-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-preview-swiper',
@@ -7,5 +9,25 @@ import { Component, Input } from '@angular/core';
   styleUrl: './preview-swiper.scss',
 })
 export class PreviewSwiper {
+  private deviceService = inject(DeviceService);
+
   @Input() productPreviewsPath!: string;
+
+  get isMobileDevice() {
+    return this.deviceService.isAndroid || this.deviceService.isiPhone;
+  }
+
+  images: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<string[]>('assets/gallery/gallery.json').subscribe((data) => {
+      this.images = data;
+    });
+  }
+
+  onClick(image: string) {
+    console.log('image', image);
+  }
 }
