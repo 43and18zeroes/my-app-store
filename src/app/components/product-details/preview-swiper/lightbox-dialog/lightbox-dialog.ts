6cf66c766981059aca0ff34094ec728a23ef063e
Christoph <c.wagner.germany@gmail.com>
2025-11-25 14:40:07 +0100

    300

--- src/app/components/product-details/preview-swiper/lightbox-dialog/lightbox-dialog.ts
+++ src/app/components/product-details/preview-swiper/lightbox-dialog/lightbox-dialog.ts
@@ -115,7 +115,7 @@ export class LightboxDialog {
         },
       ],
       {
-        duration: 500,
+        duration: 300,
         easing: 'cubic-bezier(0.4, 0.0, 0.2, 1)',
         fill: 'forwards',
       }
