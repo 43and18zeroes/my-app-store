de136ae927bebf2c7d82af73e06d9d2d1053cb92
Christoph <c.wagner.germany@gmail.com>
2025-11-29 15:26:46 +0100

    logo swiper mobile

--- src/app/components/logo-swiper/logo-swiper.ts
+++ src/app/components/logo-swiper/logo-swiper.ts
@@ -2,17 +2,15 @@ import {
   Component,
   ElementRef,
   EventEmitter,
-  inject,
   Input,
   Output,
   SimpleChanges,
   ViewChild,
 } from '@angular/core';
-import { DeviceService } from '../../services/device-service';
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
-import { NavigationOptions, SwiperOptions } from 'swiper/types';
+import { SwiperOptions } from 'swiper/types';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -28,30 +26,12 @@ export class LogoSwiper<
     productLogoAltText?: string;
   }
 > {
-  private deviceService = inject(DeviceService);
-
   @Input({ required: true }) items: T[] = [];
   @Input({ required: true }) productPath!: string;
   @Input() speedDesktop = 500;
   @Input() speedMobile = 200;
 
-  @Input() desktopBreakpoints: SwiperOptions['breakpoints'] = {
-    320: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 20 },
-    600: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 20 },
-    960: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 20 },
-    1440: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 20 },
-  };
-  @Input() mobileBreakpoints: SwiperOptions['breakpoints'] = {
-    320: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 10 },
-    600: { slidesPerView: 5, slidesPerGroup: 5, spaceBetween: 10 },
-    960: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 10 },
-  };
-
   @Output() select = new EventEmitter<T>();
-
-  get isMobileDevice() {
-    return this.deviceService.isAndroid || this.deviceService.isiPhone;
-  }
 
   @ViewChild('swiperContainer') swiperContainer!: ElementRef<HTMLElement>;
   private swiper?: Swiper;
@@ -74,15 +54,10 @@ export class LogoSwiper<
     const host = this.swiperContainer?.nativeElement;
     if (!host) return;
 
-    let nextEl: HTMLElement | undefined;
-    let prevEl: HTMLElement | undefined;
-
-    if (!this.isMobileDevice) {
-      nextEl =
-        host.querySelector<HTMLElement>('.swiper-button-next') ?? undefined;
-      prevEl =
-        host.querySelector<HTMLElement>('.swiper-button-prev') ?? undefined;
-    }
+    const nextEl =
+      host.querySelector<HTMLElement>('.swiper-button-next') ?? undefined;
+    const prevEl =
+      host.querySelector<HTMLElement>('.swiper-button-prev') ?? undefined;
 
     const baseConfig: SwiperOptions = {
       modules: [Navigation],
@@ -90,11 +65,57 @@ export class LogoSwiper<
       navigation: {
         nextEl: nextEl as HTMLElement,
         prevEl: prevEl as HTMLElement,
-      } as NavigationOptions,
-      speed: this.isMobileDevice ? this.speedMobile : this.speedDesktop,
-      breakpoints: this.isMobileDevice
-        ? this.mobileBreakpoints
-        : this.desktopBreakpoints,
+      },
+
+      speed: this.speedDesktop,
+
+      breakpoints: {
+        0: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 10,
+          speed: this.speedMobile,
+          navigation: {
+            enabled: false,
+          },
+        },
+        450: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 10,
+          speed: this.speedMobile,
+          navigation: {
+            enabled: false,
+          },
+        },
+        600: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 10,
+          speed: this.speedMobile,
+          navigation: {
+            enabled: false,
+          },
+        },
+        800: {
+          slidesPerView: 6,
+          slidesPerGroup: 6,
+          spaceBetween: 20,
+          speed: this.speedDesktop,
+          navigation: {
+            enabled: true,
+          },
+        },
+        1200: {
+          slidesPerView: 8,
+          slidesPerGroup: 8,
+          spaceBetween: 20,
+          speed: this.speedDesktop,
+          navigation: {
+            enabled: true,
+          },
+        },
+      },
     };
 
     this.destroySwiper();
