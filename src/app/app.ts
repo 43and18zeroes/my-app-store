6638ac887bedf5b6d359c2a426ab063276b3305e
Christoph <c.wagner.germany@gmail.com>
2025-11-29 15:05:25 +0100

    comments removed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -7,8 +7,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { CustomSidenav } from './components/custom-sidenav/custom-sidenav';
 import { ThemeService } from './services/theme-service';
 import { RouterOutlet } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-// import { DeviceService } from './services/device-service';
 import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
@@ -28,33 +26,18 @@ import { fromEvent, Subscription } from 'rxjs';
 })
 export class App {
   protected readonly title = signal('Angular Material Darkmode');
-  // deviceService = inject(DeviceService);
   themeService = inject(ThemeService);
   collapsed = signal(true);
   isDesktop = signal(true);
-  // private breakpointSub?: Subscription;
   private resizeSub?: Subscription;
 
-  constructor(private breakpointObserver: BreakpointObserver) {
-    // effect(() => {
-    //   console.log('app resize');
-    //   // const _width = this.deviceService.viewportWidth();
-    //   this.collapsed.set(true);
-    // });
+  constructor() {
   }
 
   ngOnInit() {
     this.themeService.initTheme();
-    // this.breakpointSub = this.breakpointObserver
-    //   .observe([Breakpoints.Handset])
-    //   .subscribe((result) => {
-    //     this.isDesktop.set(!result.matches);
-    //     this.collapsed.set(true);
-    //   });
     this.resizeSub = fromEvent(window, 'resize').subscribe(() => {
       this.collapsed.set(true);
-      // optional: nur zum Debuggen
-      // console.log('resize, collapsed -> true');
     });
   }
 
@@ -66,8 +49,4 @@ export class App {
   collapseSidenav() {
     this.collapsed.set(true);
   }
-
-  // ngOnDestroy() {
-  //   this.breakpointSub?.unsubscribe();
-  // }
 }
