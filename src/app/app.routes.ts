cfaaafeecb27b9dd79077a4447993e540ba81ecb
Christoph <c.wagner.germany@gmail.com>
2025-12-21 15:33:04 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,27 +5,32 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    title: 'My App Store',
     loadComponent: () => import('./pages/applications/applications').then((m) => m.Applications),
-  },
-  {
-    path: 'info',
-    loadComponent: () => import('./pages/info/info').then((m) => m.Info),
-  },
-  {
-    path: 'privacy-policy',
-    loadComponent: () => import('./pages/privacy-policy/privacy-policy').then((m) => m.PrivacyPolicy),
-  },
-  {
-    path: 'imprint',
-    loadComponent: () => import('./pages/imprint/imprint').then((m) => m.Imprint),
   },
   {
     path: 'start',
     pathMatch: 'full',
     redirectTo: '',
   },
+  {
+    path: 'info',
+    title: 'Info - My App Store',
+    loadComponent: () => import('./pages/info/info').then((m) => m.Info),
+  },
+  {
+    path: 'privacy-policy',
+    title: 'Privacy Policy - My App Store',
+    loadComponent: () => import('./pages/privacy-policy/privacy-policy').then((m) => m.PrivacyPolicy),
+  },
+  {
+    path: 'imprint',
+    title: 'Imprint - My App Store',
+    loadComponent: () => import('./pages/imprint/imprint').then((m) => m.Imprint),
+  },
   { 
-    path: `${PRODUCT_PATH}/:productId`, 
+    path: `${PRODUCT_PATH}/:productId`,
+    title: 'Product Details - My App Store',
     loadComponent: () => import('./components/product-details/product-details').then(m => m.ProductDetails) 
   },
   { path: '**', redirectTo: '' },
